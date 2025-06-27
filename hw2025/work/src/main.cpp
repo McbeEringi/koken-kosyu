39060615e8497094f6750c4bfd27cd937228a1df
McbeEringi <git@mcbeeringi.dev>
2025-06-27 20:33:40 +0900

    -

--- hw2025/work/src/main.cpp
+++ hw2025/work/src/main.cpp
@@ -1,13 +1,26 @@
 #include <Arduino.h>
 
+#define PIN_SER 0
+#define PIN_SERCLK 1
+#define PIN_RCLK 2
+#define PIN_OE_ 3
+
 void setup(){
-	Serial.begin(115200);
-	pinMode(4,INPUT_PULLUP);
-	pinMode(5,INPUT_PULLUP);
+	
+	pinMode(PIN_SER,OUTPUT);digitalWrite(PIN_SER,HIGH);
+	pinMode(PIN_SERCLK,OUTPUT);digitalWrite(PIN_SERCLK,HIGH);
+	pinMode(PIN_RCLK,OUTPUT);digitalWrite(PIN_RCLK,HIGH);
+	pinMode(PIN_OE_,OUTPUT);digitalWrite(PIN_OE_,LOW);
+	analogWrite(PIN_OE_,200);
+
 }
 void loop(){
-	Serial.println(
-		digitalRead(4)|
-		(digitalRead(5)<<1)
-	);
+	shiftOut(PIN_SER,PIN_SERCLK,MSBFIRST,0b11111110);
+	shiftOut(PIN_SER,PIN_SERCLK,MSBFIRST,0b10101100);
+	digitalWrite(PIN_RCLK,LOW);digitalWrite(PIN_RCLK,HIGH);
+	delay(1);
+	shiftOut(PIN_SER,PIN_SERCLK,MSBFIRST,0b01100000);
+	shiftOut(PIN_SER,PIN_SERCLK,MSBFIRST,0b01001100);
+	digitalWrite(PIN_RCLK,LOW);digitalWrite(PIN_RCLK,HIGH);
+	delay(1);
 }
